9851ab2d944c1d7192fb974940e7d8f7c85fefd9
JIYEONGSTAR <jiyeongstar@gmail.com>
2022-10-11 16:12:55 +0900

    초기세팅

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,35 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          alias: {
+            "@assets": "./assets",
+            "@constants": "./constants",
+            "@components": "./components",
+            "@hooks": "./hooks",
+            "@navigation": "./navigation",
+            "@screens": "./screens",
+            "@types": "./types",
+          },
+          extensions: [
+            ".ios.ts",
+            ".android.ts",
+            ".ts",
+            ".ios.tsx",
+            ".android.tsx",
+            ".tsx",
+            ".jsx",
+            ".js",
+            ".json",
+          ],
+        },
+      ], //절대경로
+      "babel-plugin-styled-components", //스타일드 컴포넌트 사용
+    ],
   };
 };
